9d17cd4a6370baba1c4ec6c8762c17ef8eae9217
strahinjaPC <strajo98@gmail.com>
2023-05-14 22:35:16 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <FirstRow />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import FirstRow from './components/FirstRow.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    FirstRow
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/FirstRow.vue
@@ -0,0 +1,40 @@
+<template>
+    <section id="welcomeRow star-img">
+      <div class="container-fluid text-center firstRow">
+        <div class="row center-text">
+          <h1 class="bigText">Strahinja Vidovic</h1>
+          <p class="smallText">FRONT END SOFTWARE ENGINEER</p>
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+#welcomeRow {
+  background-color: black;
+  height: 900px;
+}
+
+.center-text {
+  text-align: center;
+  margin: auto;
+}
+.first-Row {
+}
+.star-img {
+  background-image: url("../assets/nightSky.jpg");
+}
+.bigText {
+  margin: auto;
+  font-family: "Playfair Display" serif;
+  font-weight: 700;
+}
+.smallText {
+  font-family: "Montserrat", sans-serif;
+  font-weight: 300;
+}
+</style>
